e5983d5d24dab9bec80d245a928dbdb0122c915b
Ajape2 <suleimanajape232@gmail.com>
2025-11-10 11:53:44 +0100

    Delete

--- SelectionStatement.java
+++ SelectionStatement.java
@@ -1,14 +1,15 @@
 public class SelectionStatement{
-	public static void main(String[] args) {
+	public static void main(String[] args){
+		
 		int age = 18;
 		
 		//single selection statement
-		if(age >= 18) {
-			System.out.println("Wow!, you are an adult");
+		if(age >= 18){
+			System.out.println("Wow, you are an adult");
 			System.out.println("Being an adult is fun");
 		}
 		
-		//double selection statement
+		//Double selction statement
 		int num1 = 5;
 		int num2 = 25;
 		
@@ -16,6 +17,6 @@ public class SelectionStatement{
 			System.out.printf("%d is greater than %d%n",num1,num2);
 		
 		else
-		     System.out.printf("%d is greater than %d%n",num2,num1);
-    }
-}	
+			System.out.printf("%d is greater than %d%n",num2,num1);
+	}
+}
